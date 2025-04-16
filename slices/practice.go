a4df7f91c0486ce06917755b30135d27b5457647
Tejas Mane <tejasmane485@gmail.com>
2025-04-16 11:43:43 +0530

    two solved

--- slices/practice.go
+++ slices/practice.go
@@ -1,19 +1,25 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// 1) Create a new array (!) that contains three hobbies you have
+	// 		Output (print) that array in the command line.
+	hobbies := [3]string{"Travelling", "Gaming", "Coding"}
+	fmt.Println(hobbies)
+	// 2) Also output more data about that array:
+	//		- The first element (standalone)
+	fmt.Println(hobbies[0])
+	//		- The second and third element combined as a new list
+	fmt.Println(hobbies[1], hobbies[2])
+
+	// 3) Create a slice based on the first element that contains
+	//		the first and second elements.
+	//		Create that slice in two different ways (i.e. create two slices in the end)
 }
 
 // Time to practice what you learned!
 
-// 1) Create a new array (!) that contains three hobbies you have
-// 		Output (print) that array in the command line.
-// 2) Also output more data about that array:
-//		- The first element (standalone)
-//		- The second and third element combined as a new list
-// 3) Create a slice based on the first element that contains
-//		the first and second elements.
-//		Create that slice in two different ways (i.e. create two slices in the end)
 // 4) Re-slice the slice from (3) and change it to contain the second
 //		and last element of the original array.
 // 5) Create a "dynamic array" that contains your course goals (at least 2 goals)
