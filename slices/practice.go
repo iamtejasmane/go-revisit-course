57e349556aa4a80de4d8d185e71d837cb9feb007
Tejas Mane <tejasmane485@gmail.com>
2025-04-16 11:47:53 +0530

    three and four solved

--- slices/practice.go
+++ slices/practice.go
@@ -16,13 +16,20 @@ func main() {
 	// 3) Create a slice based on the first element that contains
 	//		the first and second elements.
 	//		Create that slice in two different ways (i.e. create two slices in the end)
+	s := hobbies[:2]
+	fmt.Print(s)
+	// 4) Re-slice the slice from (3) and change it to contain the second
+	//		and last element of the original array.
+
+	s = hobbies[1:]
+	fmt.Println(s)
+
+	// 5) Create a "dynamic array" that contains your course goals (at least 2 goals)
+
 }
 
 // Time to practice what you learned!
 
-// 4) Re-slice the slice from (3) and change it to contain the second
-//		and last element of the original array.
-// 5) Create a "dynamic array" that contains your course goals (at least 2 goals)
 // 6) Set the second goal to a different one AND then add a third goal to that existing dynamic array
 // 7) Bonus: Create a "Product" struct with title, id, price and create a
 //		dynamic list of products (at least 2 products).
